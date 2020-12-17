36af5c5008c042a5f99b646ab735c82a084e5d1b
Stefan Novikov <darksarksn@gmail.com>
2020-12-17 18:35:54 +0300

    create components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-const getResourse = async (url) => {
-    const res = await fetch(url);
-
-    if (!res.ok) {
-        throw new Error(`Could not fetch ${url}` + 
-        `, received ${res.status}`)
-    }
-    const body = await res.json();
-    return body;
-};
-
-
-getResourse('https://swapi.dev/api/people/2')
-    .then((body) => {
-        console.log(body);
-    })
-    .catch((err) => {
-        console.error('Could not fetch', err);
-    });
+ReactDOM.render(<App />,
+  document.getElementById('root'));
