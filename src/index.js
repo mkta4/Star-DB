0cb24e1dcf6098ef360ae661edf7b1f0b43d0f21
Stefan Novikov <darksarksn@gmail.com>
2020-12-17 14:31:33 +0300

    init star-db

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+const getResourse = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}` + 
+        `, received ${res.status}`)
+    }
+    const body = await res.json();
+    return body;
+};
+
+
+getResourse('https://swapi.dev/api/people/2')
+    .then((body) => {
+        console.log(body);
+    })
+    .catch((err) => {
+        console.error('Could not fetch', err);
+    });
